2fe58cf56d895688500fd96763cf73d6afeac597
phoban01 <phoban01@gmail.com>
2014-12-10 20:10:10 +0000

    updated python script

--- scraper.py
+++ scraper.py
@@ -7,35 +7,34 @@ def make_request(url,data_store,get_total_pages=False):
     tree = html.fromstring(page.text)
     if get_total_pages == True:
         total_pages = int(tree.xpath('//p[@class="pagLinks"]/text()')[0].split()[3])
+        if total_pages == 2:
+            total_pages += 1
     next_page_link = tree.xpath('//p[@class="pagLinks"]/a/@href')[-2]
-    img_links = tree.xpath('//div[@class="listArticle"]/a/@href')
-    img_titles = tree.xpath('//div[@class="listArticle"]/a/@title')
-    img_thumbs = tree.xpath('//div[@class="listArticle"]/a/img/@src')
+    img_links = [x.encode('utf-8') for x in tree.xpath('//div[@class="listArticle"]/a/@href')]
+    img_titles = [x.encode('utf-8') for x in tree.xpath('//div[@class="listArticle"]/a/@title')]
+    img_thumbs = [x.encode('utf-8') for x in tree.xpath('//div[@class="listArticle"]/a/img/@src')]
     data_store.extend(zip(img_titles,img_links,img_thumbs))
     return next_page_link
 
 def get_itma_thumbnails(url):
     data_store = []
-
     next_page_link = make_request(url,data_store,True)
-
     current_page = 2
 
     while current_page < total_pages:
         print next_page_link
         next_page_link = make_request(next_page_link,data_store)
         current_page += 1
-
     return data_store
 
-base_path = ""
+base_path = "/Users/piarashoban/Documents/CodingProjects/itma_webscraper/"
 base_url = "http://www.itma.ie/digitallibrary/"
 ext_urls = ["images-all","printed-items-all","interactivescores-all"]
 
 for url in ext_urls:
-    outputfile = open(base_path+"_thumbnail_data.txt","w")
+    outputfile = open(base_path+"%s_thumbnail_data.txt"%url.replace('-','_'),"w")
     scraped_data = get_itma_thumbnails(base_url+url)
-    for i in scraped_data:
-        outputfile.write("%s\t%s\t%s\n" % i)
+    for i,data in enumerate(scraped_data):
+        outputfile.write("%s\t%s , %s , %s\n" % (i+1,data[0],data[1],data[2]))
     outputfile.close()
 
